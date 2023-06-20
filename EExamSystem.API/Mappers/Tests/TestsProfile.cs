4cc74ff46e134a9ccb57567780380c03a2ebf153
Akraminum <Akraminum@gmail.com>
2023-06-20 14:34:00 +0300

    init

--- EExamSystem.API/Mappers/Tests/TestsProfile.cs
+++ EExamSystem.API/Mappers/Tests/TestsProfile.cs
@@ -1,5 +1,5 @@
 ﻿using AutoMapper;
-using EExamSystem.Core.Dtos.Test;
+using EExamSystem.Core.Dtos.Tests;
 using EExamSystem.Infrastructure.Entities.Tests;
 using System;
 using System.Collections.Generic;
